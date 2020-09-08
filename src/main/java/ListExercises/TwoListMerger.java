8dcdf2969acc9139d77af2ece7a95118822fbad1
Alberto de Blas <alberto.de.blas@gmail.com>
2020-09-08 19:28:47 +0200

    refactor: Refactor mergeTwoLinkedLists
    Refactor mergeTwoLinkedLists by creating addElementToMergedList method to remove duplication

--- src/main/java/ListExercises/TwoListMerger.java
+++ src/main/java/ListExercises/TwoListMerger.java
@@ -7,36 +7,33 @@ public class TwoListMerger {
         ListNode<Integer> headResult = mergedList;
 
         while(l1 != null || l2 != null){
-
             if(l1 != null && l2 != null){
                 if(l1.value.compareTo(l2.value) < 0){
-                    mergedList.next = l1;
-                    mergedList = l1;
+                    mergedList = addElementToMergedList(l1, mergedList);
                     l1 = l1.next;
                 }else{
-                    mergedList.next = l2;
-                    mergedList = l2;
+                    mergedList = addElementToMergedList(l2, mergedList);
                     l2 = l2.next;
                 }
             }else if(l1 != null){
-                mergedList.next = l1;
-                mergedList = l1;
+                mergedList = addElementToMergedList(l1, mergedList);
                 l1 = l1.next;
             }else if(l2 != null){
-                mergedList.next = l2;
-                mergedList = l2;
+                mergedList = addElementToMergedList(l2, mergedList);
                 l2 = l2.next;
             }
-
         }
-
         return headResult.next;
+    }
 
+    private static ListNode<Integer> addElementToMergedList(ListNode<Integer> l2, ListNode<Integer> mergedList) {
+        mergedList.next = l2;
+        mergedList = l2;
+        return mergedList;
     }
 
     public ListNode<Integer> mergeNode(ListNode<Integer> mergedList, ListNode<Integer> node){
-        mergedList.next = node;
-        mergedList = node;
+        mergedList = addElementToMergedList(node, mergedList);
         return node.next;
     }
 }
