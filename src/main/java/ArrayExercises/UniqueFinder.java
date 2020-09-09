58a11b9e74b70cf9bf4ef6cc03a3c4e72ad6700f
Alberto de Blas <alberto.de.blas@gmail.com>
2020-09-09 20:19:27 +0200

    refactor: Refactor UniqueFinder
    refactor: Refactor UniqueFinder

--- src/main/java/ArrayExercises/UniqueFinder.java
+++ src/main/java/ArrayExercises/UniqueFinder.java
@@ -7,24 +7,36 @@ public class UniqueFinder {
 
         int baseline_ascii = (int) 'a';
         int bottom_ascii = (int) 'z';
-
         int[] counts = new int[bottom_ascii - baseline_ascii+1];
-        ArrayList<Integer> appearanceOrder = new ArrayList();
 
-        for(int i = 0; i < s.length();i++){
-            counts[(int)s.charAt(i)-baseline_ascii]++;
+        ArrayList<Integer> appearanceOrder = createAppearanceOrderList(s, baseline_ascii, counts);
+        Character item = findFirstItemWithOneAppearance(baseline_ascii, counts, appearanceOrder);
+        return (item != null) ?  item :  '_';
+    }
 
-            if (!appearanceOrder.contains(s.charAt(i)-baseline_ascii)){
-                appearanceOrder.add(s.charAt(i)-baseline_ascii);
-            }
-        }
-
+    private static Character findFirstItemWithOneAppearance(int baseline_ascii, int[] counts, ArrayList<Integer> appearanceOrder) {
         for (Integer item : appearanceOrder) {
             if (counts[item]== 1) {
-                return (char)(item+baseline_ascii);
+                return (char)(item+ baseline_ascii);
             }
         }
+        return null;
+    }
 
-        return '_';
+    private static ArrayList<Integer> createAppearanceOrderList(String s, int baseline_ascii, int[] counts) {
+        ArrayList<Integer> appearanceOrder = new ArrayList();
+
+        for(int i = 0; i < s.length(); i++){
+            addAppearance(s, baseline_ascii, counts, appearanceOrder, i);
+        }
+        return appearanceOrder;
+    }
+
+    private static void addAppearance(String s, int baseline_ascii, int[] counts, ArrayList<Integer> appearanceOrder, int i) {
+        counts[(int) s.charAt(i)- baseline_ascii]++;
+
+        if (!appearanceOrder.contains(s.charAt(i)- baseline_ascii)){
+            appearanceOrder.add(s.charAt(i)- baseline_ascii);
+        }
     }
 }
